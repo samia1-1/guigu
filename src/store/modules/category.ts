3858f0d1e3ff73e2f620282e2475581c8ee54a79
samia1-1 <1744151634@qq.com>
2025-04-04 15:16:35 +0800

    1

--- src/store/modules/category.ts
+++ src/store/modules/category.ts
@@ -1,41 +1,51 @@
-import { defineStore } from "pinia";
-import { reqC1, reqC2, reqC3 } from "@/api/product/attr/index.js";
-import type { CategoryResponseData } from "@/api/product/attr/type.js";
-import type { CategoryState } from "@/store/modules/types/types.js";
-let usecategoryStore = defineStore("category", {
+//商品分类全局组件的小仓库
+import { defineStore } from 'pinia'
+import { reqC1, reqC2, reqC3 } from '@/api/product/attr/index.js'
+import type { CategoryResponseData } from '@/api/product/attr/type.js'
+import type { CategoryState } from './types/type.js'
+const useCategoryStore = defineStore('Category', {
   state: (): CategoryState => {
     return {
-      c1Arr: [], // 一级分类数据
-      c2Arr: [], // 二级分类数据
-      c3Arr: [], // 三级分类数据
-      c1Id: "", // 一级分类ID
-      c2Id: "", // 二级分类ID
-      c3Id: "", // 三级分类ID
-    };
+      //存储一级分类的数据
+      c1Arr: [],
+      //存储一级分类的ID
+      c1Id: '',
+      //存储对应一级分类下二级分类的数据
+      c2Arr: [],
+      //收集二级分类的ID
+      c2Id: '',
+      //存储三级分类的数据
+      c3Arr: [],
+      //存储三级分类的ID
+      c3Id: '',
+    }
   },
   actions: {
+    //获取一级分类的方法
     async getC1() {
-      let result: CategoryResponseData = await reqC1();
-      if (result.code === 200) {
-        this.c1Arr = result.data;
-      } else {
+      //发请求获取一级分类的数据
+      const result: CategoryResponseData = await reqC1()
+      if (result.code == 200) {
+        this.c1Arr = result.data
       }
     },
+    //获取二级分类的数据
     async getC2() {
-      let result: CategoryResponseData = await reqC2(this.c1Id);
-      if (result.code === 200) {
-        this.c2Arr = result.data;
-      } else {
+      //获取对应一级分类的下二级分类的数据
+      const result: CategoryResponseData = await reqC2(this.c1Id)
+      if (result.code == 200) {
+        this.c2Arr = result.data
       }
     },
+    //获取三级分类的数据
     async getC3() {
-      let result: CategoryResponseData = await reqC3(this.c2Id);
-      if (result.code === 200) {
-        this.c3Arr = result.data;
-      } else {
+      const result: CategoryResponseData = await reqC3(this.c2Id)
+      if (result.code == 200) {
+        this.c3Arr = result.data
       }
     },
   },
-});
+  getters: {},
+})
 
-export default usecategoryStore;
+export default useCategoryStore
--- src/store/modules/setting.ts
+++ src/store/modules/setting.ts
@@ -1,12 +1,13 @@
-import { defineStore } from "pinia";
+//小仓库:layout组件相关配置仓库
+import { defineStore } from 'pinia'
 
-let useLayOutSettingStore = defineStore("SettingStore", {
+const useLayOutSettingStore = defineStore('SettingStore', {
   state: () => {
     return {
-      fold: false,
-      refresh: false,
-    };
+      fold: false, //用户控制菜单折叠还是收起控制
+      refsh: false, //仓库这个属性用于控制刷新效果
+    }
   },
-});
+})
 
-export default useLayOutSettingStore;
+export default useLayOutSettingStore
